fad78e5e16508e8563584c268788a1fb7cc1280d
jviveiros <joe.viveiros@verygood.systems>
2017-11-15 09:59:31 -0800

    Checkpoint

--- scala/scala-example/src/main/scala/example/Example.scala
+++ scala/scala-example/src/main/scala/example/Example.scala
@@ -2,9 +2,7 @@ import faker._
 import scalaj.http._
 import spray.json._
 import DefaultJsonProtocol._
-import java.nio.charset.Charset
 import java.nio.charset.StandardCharsets.UTF_8
-
 
 object Proxies extends App {
 
@@ -12,6 +10,12 @@ object Proxies extends App {
   val password = sys.env("FORWARD_HTTP_PROXY_PASSWORD")
   val reverseProxy = sys.env("REVERSE_HTTP_PROXY_HOST")
   val forwardProxy = sys.env("FORWARD_HTTP_PROXY_HOST")
+
+
+  def base64(bytes: Array[Byte]): String = new String(Base64.encode(bytes))
+  def base64(in: String): String = base64(in.getBytes(UTF_8))
+
+  val proxyAuth = base64(username + ":" + password)
 
   //Random Name for data payload
   val fake = Name.name
@@ -24,13 +28,20 @@ object Proxies extends App {
   val json = (response.body).parseJson
   val redacted = json.convertTo[Map[String, JsValue]]
   val redactedSecret = redacted.get("json").get.toString
+  println(redactedSecret)
 
   //Forward forwardProxy
-  val url: String = "https://httpbin.verygoodsecurity.io/post"
-  val proxyHost= s"https://$username:$password@$forwardProxy"
-  val requestForward = Http(url).proxy(proxyHost, 8080).option(HttpOptions.allowUnsafeSSL).postData(redactedSecret).header("Content-type", "application/json")
-  val responseForward: HttpResponse[String] = requestForward.asString
-  val forward_json = (responseForward.body).parseJson
+  val url = "https://httpbin.verygoodsecurity.io/post"
+
+  val proxyHost= s"https://$forwardProxy"
+  val requestForward = Http(url).postData(redactedSecret)
+    .option(HttpOptions.allowUnsafeSSL)
+    .header("Content-Type", "application/json")
+    .param("Proxy-Authorization", s"Basic $proxyAuth")
+    .proxy(proxyHost, 8080).asString
+  val responseForward: HttpResponse[String] = requestForward
+  val forwardJson = (responseForward.body).parseJson
+  println(forwardJson)
   val revealed = json.convertTo[Map[String, JsValue]]
   val revealedSecret = revealed.get("json").get
 
