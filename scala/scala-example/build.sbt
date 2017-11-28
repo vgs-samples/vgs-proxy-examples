348f8514e9a7fe6d83d7d4cc393d3f858855fd9b
Phuong H <phuonghqh@gmail.com>
2017-11-28 23:29:12 +0700

    Fix Scala SSL

--- scala/scala-example/build.sbt
+++ scala/scala-example/build.sbt
@@ -1,16 +1,20 @@
 import Dependencies._
 
 lazy val root = (project in file(".")).
-  settings(
-    inThisBuild(List(
-      organization := "com.verygoodsecurity",
-      scalaVersion := "2.12.3",
-      version      := "0.1.0-SNAPSHOT"
+    settings(
+        inThisBuild(List(
+            organization := "com.verygoodsecurity",
+            scalaVersion := "2.12.3",
+            version := "0.1.0-SNAPSHOT"
+        )
+        )
     )
-  )
+
+libraryDependencies ++= Seq(
+    "it.bitbl" %% "scala-faker" % "0.4",
+    "com.softwaremill.sttp" %% "json4s" % "1.1.0",
+    "com.softwaremill.sttp" %% "core" % "1.1.0",
+    "com.softwaremill.sttp" %% "okhttp-backend" % "1.1.0"
+
 )
 
-  libraryDependencies += "org.scalaj" %% "scalaj-http" % "2.3.0"
-  libraryDependencies += "it.bitbl" %% "scala-faker" % "0.4"
-  libraryDependencies += "io.spray" %%  "spray-json" % "1.3.3"
-  libraryDependencies += "com.typesafe.play" %% "play-ahc-ws-standalone" % "1.1.3"
--- scala/scala-example/src/main/scala/example/Example.scala
+++ scala/scala-example/src/main/scala/example/Example.scala
@@ -1,69 +1,87 @@
+package example
+
 import faker._
-import scalaj.http._
-import spray.json._
-import DefaultJsonProtocol._
-import java.net.{Authenticator,PasswordAuthentication}
-import java.nio.file.{Files, Path, Paths, FileSystems}
-import javax.net.ssl._
-import scala.collection.JavaConversions._
+import java.net.{InetSocketAddress, Proxy}
+import com.softwaremill.sttp.okhttp.{OkHttpSyncBackend}
+import okhttp3.{Authenticator, OkHttpClient}
 
-class Proxies  {
-  import Proxies._
-  val resourcesPath = getClass.getResource("cacerts")
-  val fullPath = resourcesPath.getPath
-  System.setProperty("javax.net.ssl.trustStore", Proxies.getClass.getClassLoader.getResource("cacerts").getFile())
-  System.setProperty("javax.net.ssl.trustStorePassword", "password")
-  System.setProperty("javax.net.ssl.trustStoreType", "JKS")
-}
+import okhttp3.Credentials
+import okhttp3.Route
+import okhttp3.Response
+import okhttp3.Request
+import java.io.IOException
+import com.softwaremill.sttp._
 
-object Proxies extends App {
+import org.json4s._
+import org.json4s.native.JsonMethods._
 
-  //Get Env variables
-  val username = sys.env("FORWARD_HTTP_PROXY_USERNAME")
-  val password = sys.env("FORWARD_HTTP_PROXY_PASSWORD")
-  val reverseProxy = sys.env("REVERSE_HTTP_PROXY_HOST")
-  val forwardProxy = sys.env("FORWARD_HTTP_PROXY_HOST")
+object Example extends App {
+
+    //Get Env variables
+    val username = sys.env("FORWARD_HTTP_PROXY_USERNAME")
+    val password = sys.env("FORWARD_HTTP_PROXY_PASSWORD")
+    val reverseProxy = sys.env("REVERSE_HTTP_PROXY_HOST")
+    val forwardProxy = sys.env("FORWARD_HTTP_PROXY_HOST")
+
+    def redactViaReverseProxy(originalData: String): String = {
+        implicit val backend: SttpBackend[Id, _] = OkHttpSyncBackend()
+
+        val request = sttp
+            .body(originalData)
+            .post(uri"https://$reverseProxy/post")
+
+        val json = parse(request.send.unsafeBody)
+
+        implicit val formats: DefaultFormats = DefaultFormats
+        (json \ "data").extract[String]
+    }
+
+    def revealViaForwardProxy(redactedSecret: String): String = {
+        System.setProperty("javax.net.ssl.trustStore", this.getClass.getClassLoader.getResource("cacerts").getFile)
+        System.setProperty("javax.net.ssl.trustStorePassword", "password")
+        System.setProperty("javax.net.ssl.trustStoreType", "JKS")
+
+        val proxyAuthenticator = new Authenticator() {
+            @throws[IOException]
+            def authenticate(route: Route, response: Response): Request = {
+                val credential = Credentials.basic(username, password)
+                response.request.newBuilder.header("Proxy-Authorization", credential).build
+            }
+        }
+
+        val hostport: Array[String] = forwardProxy.split(":")
+        val client = new OkHttpClient.Builder()
+            .proxy(new Proxy(Proxy.Type.HTTP, new InetSocketAddress(hostport(0), hostport(1).toInt)))
+            .proxyAuthenticator(proxyAuthenticator)
+            .build()
 
 
-  //Random Name to JSON for data payload
-  val fake = Name.name
-  def data = s"""{"secret":"$fake"}"""
-  println(data)
+        implicit val backend: SttpBackend[Id, _] = OkHttpSyncBackend.usingClient(client)
 
-  //Reverse Proxy
-  val reverseUrl: String = (s"https://$reverseProxy/post")
-  def request = Http(reverseUrl).postData(data).header("Content-type", "application/json")
-    val response: HttpResponse[String] = request.asString
-    val json = (response.body).parseJson
-    val redacted = json.convertTo[Map[String, JsValue]]
-    val redactedSecret = redacted.get("json").get.toString
+        val request = sttp
+            .body(redactedSecret)
+            .post(uri"https://httpbin.verygoodsecurity.io/post")
+
+        val json = parse(request.send.unsafeBody)
+
+        implicit val formats: DefaultFormats = DefaultFormats
+        (json \ "data").extract[String]
+    }
+
+    //Random Name to JSON for data payload
+    val fake = Name.name
+
+    def originalValue = s"""{"secret":"$fake"}"""
+    println(originalValue)
+
+    val redactedSecret = Example.redactViaReverseProxy(originalValue)
     println(redactedSecret)
 
-  //Forward Proxy
+    val revealedValue = Example.revealViaForwardProxy(redactedSecret)
+    println(revealedValue)
 
-  val url = "https://httpbin.verygoodsecurity.io/post"
-  val proxyHost= s"https://$forwardProxy"
-  def requestForward: HttpRequest = Http(url).postData(data)
-    .header("Content-Type", "application/json")
-    .proxy(proxyHost, 8080)
-    .option(HttpOptions.allowUnsafeSSL)
-
-  Authenticator.setDefault(new Authenticator() {
-   override def getPasswordAuthentication(): PasswordAuthentication = {
-    new PasswordAuthentication( s"$username", s"$password".toCharArray())
-   }
-  })
-
-    val responseForward: HttpResponse[String] = requestForward.asString
-    val forwardJson = (responseForward.body).parseJson
-    val revealed = forwardJson.convertTo[Map[String, JsValue]]
-    val revealedSecret = revealed.get("json").get
-    println(revealedSecret)
-
-  //Tests
-   assert(s"$data" != s"$redactedSecret")
-   assert(s"$redactedSecret" != s"$revealedSecret")
-   assert(s"$data" == s"$revealedSecret")
-  
-
+    //Tests
+    assert(s"$originalValue" != s"$redactedSecret")
+    assert(s"$redactedSecret" != s"$revealedValue")
+    assert(s"$revealedValue" == s"$revealedValue")
 }
